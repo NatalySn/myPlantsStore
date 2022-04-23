2e7720007a7f4f2898ce89a0e43c03aaacebf583
NatalySn <zelenucha3@gmail.com>
2022-04-23 13:01:11 +0300

    added loading by scrollig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule,
+  
+    HttpClientModule,
+
+   
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
